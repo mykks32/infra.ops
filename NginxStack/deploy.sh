a1ddbf2ec1200b3229de79ffadb214d39eaa5be7
Shree Krishna Yadav <srikri5462ydv@gmail.com>
2026-03-21 15:51:54 +0545

    fix(NginxStack): k8s deployments fix

--- NginxStack/deploy.sh
+++ NginxStack/deploy.sh
@@ -5,6 +5,14 @@ docker build -t backend -f ./docker/backend.Dockerfile .
 
 docker build -t frontend -f ./docker/frontend.Dockerfile .
 
+echo "Tagging images for Docker Hub..."
+docker tag localhost/backend mykks32/backend:latest
+docker tag localhost/frontend mykks32/frontend:latest
+
+echo "Pushing images to Docker Hub..."
+docker push mykks32/backend:latest
+docker push mykks32/frontend:latest
+
 echo "Deploying in Kubernetes..."
 kubectl apply -f k8s/
 
