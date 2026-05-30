9d82a8b0881a2070834802d0831801f242963ce1
Shree Krishna Yadav <srikri5462ydv@gmail.com>
2026-05-31 03:19:22 +0545

    feat(microservice): exceptions

---
+++ books-microservices/book-mongo-service/src/common/exceptions/http.exception.ts
@@ -0,0 +1,44 @@
+import {
+  HttpErrorCodeMessage,
+  type HttpErrorCodeTypes,
+} from '../utils/http-error.code';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Exclude,
+  Expose,
+  Transform,
+  TransformFnParams,
+} from 'class-transformer';
+
+export class GlobalHttpException extends HttpException {
+  @Expose() requestId: string;
+  @Expose() statusCode: HttpStatus;
+  @Expose() errorCode: HttpErrorCodeTypes;
+  @Expose() message: string;
+  @Expose()
+  @Transform(
+    ({ obj }: TransformFnParams) => (obj as GlobalHttpException).additional,
+  )
+  additional?: object;
+  @Expose()
+  @Transform(
+    ({ obj }: TransformFnParams) => (obj as GlobalHttpException).payload,
+  )
+  payload: object;
+
+  constructor(
+    errorCode: HttpErrorCodeTypes,
+    httpStatus: HttpStatus,
+    additional?: object,
+  ) {
+    const message = HttpErrorCodeMessage[errorCode];
+    super(message, httpStatus);
+
+    this.errorCode = errorCode;
+    this.statusCode = httpStatus;
+    this.additional = additional;
+    this.message = message;
+  }
+
+  @Exclude() name: string;
+}
